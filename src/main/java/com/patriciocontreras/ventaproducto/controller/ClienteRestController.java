6e025b6289a2dec3444f3f7c1cef198e4cef8aa0
patorma <pocontreras@ing.ucsc.cl>
2021-08-10 11:16:36 -0400

    se termino de crear los metodos: create(), update() y delete()

--- src/main/java/com/patriciocontreras/ventaproducto/controller/ClienteRestController.java
+++ src/main/java/com/patriciocontreras/ventaproducto/controller/ClienteRestController.java
@@ -3,6 +3,9 @@ package com.patriciocontreras.ventaproducto.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -11,9 +14,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -61,7 +69,112 @@ public class ClienteRestController {
 		
 	}
 	
-	// aqui quede
+	@PostMapping("/clientes")
+	public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente,BindingResult result){
+		
+		Cliente clienteNew = null;
+		
+		Map<String, Object> response = new HashMap<>();
+		
+		// se valida si contiene errores el objeto 
+		if(result.hasErrors()) {
+			// se debe obtener los mensajes de errror de cada campo 
+						// y convertir estos en una lista de errores de tipo string
+						// se debe convertir esta lista de fielderrors en String
+						List<String> errors = result.getFieldErrors()
+								.stream()
+								.map(err -> "El campo '"+ err.getField() + "' "+err.getDefaultMessage())//muy parecido  al operador map en angular (rxjs), mismo concepto!
+								.collect(Collectors.toList());// ahora podemos convertir de regreso el stream  aun tipo List
+						response.put("errors", errors);
+						// se responde con un responseentity con listados de error
+						return new ResponseEntity<Map<String, Object>>(response,HttpStatus.BAD_REQUEST);
+						// en lo anterior se recibe un field errors y lo convertimos a string
+		}
+		
+		try {
+			clienteNew = clienteService.save(cliente);
+		}catch(DataAccessException e) {
+			response.put("mensaje", "Error al realizar el insert en la base de datos!");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		//se podria pasar un map con un mensaje y con el clientecreado
+				response.put("mensaje", "El cliente ha sido creado con éxito! ");
+				response.put("cliente",clienteNew);
+				return new ResponseEntity<Map<String, Object>>(response,HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/clientes/{id}")
+	public ResponseEntity<?> update(@Valid @RequestBody Cliente cliente,BindingResult result,@PathVariable Long id){
+
+		//Obtenemos el cliente que queremos modificar de la bd por Id
+		Cliente clienteActual = clienteService.findById(id);
+		
+		//Cliente que ya fue actualizado
+		Cliente clienteUpdated = null;
+		Map<String, Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+			// se debe obtener los mensajes de errror de cada campo 
+			// y convertir estos en una lista de errores de tipo string
+			
+			// se debe convertir esta lista de fielderrors en String
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					.map(err -> "El campo '"+ err.getField() + "' "+err.getDefaultMessage())// muy parecido  al operador map en angular (rxjs), mismo concepto!
+					.collect(Collectors.toList());// ahora podemos convertir de regreso el stream  aun tipo List
+			response.put("errors", errors);
+			// se responde con un responseentity con listados de error
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.BAD_REQUEST);
+			
+			// en lo anterior se recibe un field errors y lo convertimos a string
+		}
+		
+		if(clienteActual == null) {
+			response.put("mensaje", "Error: no se pudo editar, el cliente con ID: ".concat(id.toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.NOT_FOUND);
+		}
+		
+		try {
+			clienteActual.setNombre(cliente.getNombre());
+			clienteActual.setApellido(cliente.getApellido());
+			clienteActual.setTelefono(cliente.getTelefono());
+			clienteActual.setDireccion(cliente.getDireccion());
+			
+			 clienteUpdated = clienteService.save(clienteActual);
+			
+		}catch(DataAccessException e) {
+			response.put("mensaje", "Error al actualizar el cliente en la base de datos!");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mensaje", "El cliente ha sido actualizado con éxito!");
+		response.put("cliente", clienteUpdated);
+		
+		return new ResponseEntity<Map<String, Object>>(response,HttpStatus.CREATED);
+	}
+	
+	
+	@DeleteMapping("/clientes/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id){
+		
+		//Map para guardar el contenido que enviaremos en el ResponseEntity con mensajes
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			clienteService.delete(id);
+		}catch (DataAccessException e) {
+			response.put("mensaje", "Error al eliminar el cliente de la base de datos!");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mensaje", "El cliente fue eliminado con éxito!");
+		return new ResponseEntity<Map<String, Object>>(response,HttpStatus.OK);
+		
+	}
 	
 
 }
