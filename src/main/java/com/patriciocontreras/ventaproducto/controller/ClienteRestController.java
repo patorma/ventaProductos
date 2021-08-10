bd93d91ce9a38c1eba9c02356e70467c36dd38dc
patorma <pocontreras@ing.ucsc.cl>
2021-08-10 01:41:14 -0400

    se implmento los metodos index, index para paginador y show para ver un solo cliente por id

--- src/main/java/com/patriciocontreras/ventaproducto/controller/ClienteRestController.java
+++ src/main/java/com/patriciocontreras/ventaproducto/controller/ClienteRestController.java
@@ -1,10 +1,23 @@
 package com.patriciocontreras.ventaproducto.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.patriciocontreras.ventaproducto.models.entity.Cliente;
 import com.patriciocontreras.ventaproducto.models.service.IClienteService;
 
 @RestController
@@ -15,6 +28,40 @@ public class ClienteRestController {
 	@Autowired
 	private IClienteService clienteService;
 	
+	@GetMapping("/clientes")
+	public List<Cliente> index(){
+		return clienteService.findAll();
+	}
+	
+	@GetMapping("/clientes/page/{page}")
+	public Page<Cliente> index(@PathVariable Integer page){
+		Pageable pageable = PageRequest.of(page, 4);
+		return clienteService.findAll(pageable);
+		
+	}
+	
+	@GetMapping("/clientes/{id}")
+	public ResponseEntity<?> show(@PathVariable Long id){
+		Cliente cliente = null;
+		Map<String, Object> response  = new HashMap<>();
+		try {
+			cliente = clienteService.findById(id);
+		}catch(DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta en la base de datos!");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		if(cliente == null) {
+			response.put("mensaje", "El cliente con ID: ".concat(id.toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<Cliente>(cliente,HttpStatus.OK);
+		
+	}
+	
+	// aqui quede
 	
 
 }
